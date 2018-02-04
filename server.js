fb9c0385d96651128ea8a636b4805da45d0cf96d
Kenjiutsu <rafael.andres.martell@gmail.com>
2018-02-04 15:40:56 -0500

    about.html fix attempt

--- server.js
+++ server.js
@@ -79,12 +79,7 @@ app.get('/', function (req, res) {
 
 app.get('/about', function (req, res) {
 
-    // try to initialize the db on every request if it's not already
-    // initialized.
-    if (!db) {
-        initDb(function(err){});
-    }
-    res.send('about.html');
+    res.render('about.html');
 });
 
 app.get('/pagecount', function (req, res) {
