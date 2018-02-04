96e8cf62ee97e4b4a3064be25079951c6ec3da22
Kenjiutsu <rafael.andres.martell@gmail.com>
2018-02-04 13:15:50 -0500

    About Page Init

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
-    
+
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
@@ -75,6 +75,30 @@ app.get('/', function (req, res) {
   } else {
     res.render('index.html', { pageCountMessage : null});
   }
+});
+
+app.get('/about', function (req, res) {
+
+    // try to initialize the db on every request if it's not already
+    // initialized.
+    if (!db) {
+        initDb(function(err){});
+    }
+    if (db) {
+        var col = db.collection('counts');
+
+        // Create a document with request IP and current time of request
+        col.insert({ip: req.ip, date: Date.now()});
+        col.count(function(err, count){
+
+        if (err) {
+            console.log('Error running count. Message:\n'+err);
+        }
+        res.render('about.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+    } else {
+        res.render('about.html', { pageCountMessage : null});
+    }
 });
 
 app.get('/pagecount', function (req, res) {
